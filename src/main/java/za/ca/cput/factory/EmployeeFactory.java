3669fdee60553ea7709c243c73b13ecb9d078c3c
Argus <argushakizimana@gmail.com>
2023-08-22 15:48:09 +0200

    stuff deleted not to be merged into maste

--- src/main/java/za/ca/cput/factory/EmployeeFactory.java
+++ src/main/java/za/ca/cput/factory/EmployeeFactory.java
@@ -1,24 +1,22 @@
 package za.ca.cput.factory;
 
 import za.ca.cput.domain.Employee;
-import za.ca.cput.util.Helper;
 
 public class EmployeeFactory {
     public  static Employee createEmployee(String firstName, String lastName, String email) {
-
-        if (Helper.isNullOrEmpty(firstName) || Helper.isNullOrEmpty(lastName)) {
-            return null;
-        }
-        String employeeNumber = Helper.generateId();
-
-        if(!Helper.isValid(email)){
-            return null;
-        }
+//
+//        if (Helper.isNullOrEmpty(firstName) || Helper.isNullOrEmpty(lastName)) {
+//            return null;
+//        }
+//        String employeeNumber = Helper.generateId();
+//
+//        if(!Helper.isValid(email)){
+//            return null;
+//        }
 
         Employee employee = new Employee.Builder()
                 .setFirstName(firstName)
                 .setLastName(lastName)
-                .setEmployeeNumber(employeeNumber)
                 .setEmail(email)
                 .build();
 
